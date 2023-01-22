bc820f508b7cc5906e7612bb6d5b621269d1e668
Leandro Schillreff <leandroschillreff@gmail.com>
2023-01-22 14:21:45 -0400

    feat: added container for aplication

---
+++ src/components/Container/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  /* max-width: 1440px; */
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 1rem 1rem;
+`;
